8efd566b4f6ca2ba6897010a287f19aceb096d68
tray <at-box@mail.ru>
2026-06-26 08:41:35 +0500

    library views for books_list and book

--- library/views.py
+++ library/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from library.models import Book
+
 
 # Create your views here.
+def books_list(request):
+    books = Book.objects.all()
+    return render(request, "books_list.html", {"books": books})
+
+
+def book(request, pk):
+    book = Book.objects.get(pk=pk)
+    return render(request, "book_details.html", {"book": book})
