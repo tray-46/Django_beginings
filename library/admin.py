d6fdc0efedbeea74435bdddd3eaa3b9b21aba9f6
tray <at-box@mail.ru>
2026-06-26 08:17:20 +0500

    library admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Author, Book
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "birth_date",)
+    search_fields = ("last_name", "first_name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "publication_data",)
+    list_filter = ("author",)
+    search_fields = ("author__last_name", "author__first_name", "title",)
