12a1bd6dea1ad25d2fbc35a1eae56df49e3d36e7
tray <at-box@mail.ru>
2026-06-26 08:16:01 +0500

    library models and fixtures

--- library/models.py
+++ library/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    birth_date = models.DateField(verbose_name="Дата рождения")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "автор"
+        verbose_name_plural = "авторы"
+        ordering = ["last_name", "first_name"]
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=200, verbose_name="Название")
+    publication_data = models.DateField(verbose_name="Дата публикации")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "книга"
+        verbose_name_plural = "книги"
+        ordering = ["title"]
