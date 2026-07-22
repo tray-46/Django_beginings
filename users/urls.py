fa36807bd5be6c498ff9dfd10bfd6ba688782d3d
tray <at-box@mail.ru>
2026-07-22 14:20:02 +0500

    urls

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from users import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+]
